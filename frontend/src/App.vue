f3c3978677c924d07babc4d34ed443f81f774d8f
cuauhizo <cuauhizo@gmail.com>
2025-08-19 10:39:48 -0600

    inactividad

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,52 @@
 <script setup>
+import { onMounted, onBeforeUnmount, ref } from 'vue';
 import { useAuthStore } from './stores/auth.js';
+import { useRouter } from 'vue-router';
+import { useFeedback } from './composables/useFeedback.js';
+
 const authStore = useAuthStore();
+const router = useRouter();
+const { feedbackMessage, feedbackType, showFeedback } = useFeedback();
+
+// Temporizador para manejar la inactividad del usuario
+let timeout;
+const INACTIVITY_TIME = 1000 * 60 * 15; // 15 minutos en milisegundos
+// const INACTIVITY_TIME = 3000; // 15 minutos en milisegundos
+
+// Función para reiniciar el temporizador de inactividad
+const resetTimer = () => {
+  clearTimeout(timeout);
+  console.log(timeout);
+  if (authStore.isAuthenticated) {
+    timeout = setTimeout(logoutDueToInactivity, INACTIVITY_TIME);
+  }
+};
+
+// Función para manejar el cierre de sesión por inactividad
+const logoutDueToInactivity = () => {
+  console.log('Sesión cerrada por inactividad.');
+  authStore.logout();
+  router.push({ name: 'login' });
+  showFeedback('Su sesión se cerró por inactividad. Por favor, inicie sesión de nuevo.', 'error');
+};
+
+// Configurar los listeners de eventos para detectar actividad del usuario
+onMounted(() => {
+  window.addEventListener('mousemove', resetTimer);
+  window.addEventListener('keypress', resetTimer);
+  window.addEventListener('click', resetTimer);
+  window.addEventListener('scroll', resetTimer);
+  resetTimer(); // Iniciar el temporizador al cargar la aplicación
+});
+
+// Limpiar los listeners antes de desmontar el componente para evitar fugas de memoria
+onBeforeUnmount(() => {
+  clearTimeout(timeout);
+  window.removeEventListener('mousemove', resetTimer);
+  window.removeEventListener('keypress', resetTimer);
+  window.removeEventListener('click', resetTimer);
+  window.removeEventListener('scroll', resetTimer);
+});
 </script>
 
 <template>
@@ -45,6 +91,9 @@ const authStore = useAuthStore();
     <main class="container-xxxl py-4">
       <router-view />
     </main>
+     <div v-if="feedbackMessage" :class="['feedback-message', feedbackType]">
+      {{ feedbackMessage }}
+    </div>
   </div>
 </template>
 
